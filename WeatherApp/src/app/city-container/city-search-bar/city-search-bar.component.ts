418115aab4737899c340d9baca827f7476deafc3
VoinaRares <voinarares14@gmail.com>
2025-05-12 19:30:13 +0300

    feat: integrate city search functionality with Google Places API and enhance city management UI

---
+++ WeatherApp/src/app/city-container/city-search-bar/city-search-bar.component.ts
@@ -0,0 +1,61 @@
+import {
+  Component,
+  AfterViewInit,
+  ViewChild,
+  ElementRef,
+  NgZone,
+} from '@angular/core';
+import { CityService } from '../city.service';
+
+declare const google: any;
+
+@Component({
+  selector: 'app-city-search-bar',
+  templateUrl: './city-search-bar.component.html',
+  styleUrl: './city-search-bar.component.scss',
+})
+export class CitySearchBarComponent implements AfterViewInit {
+  @ViewChild('searchBox') searchBox!: ElementRef;
+
+  selectedCityName: string = '';
+  selectedLat: number = 0;
+  selectedLng: number = 0;
+
+  constructor(private ngZone: NgZone, private cityService: CityService) {}
+
+  ngAfterViewInit(): void {
+    const autocomplete = new google.maps.places.Autocomplete(
+      this.searchBox.nativeElement,
+      {
+        types: ['(cities)'],
+      }
+    );
+
+    autocomplete.addListener('place_changed', () => {
+      this.ngZone.run(() => {
+        const place = autocomplete.getPlace();
+
+        if (place.geometry && place.geometry.location && place.name) {
+          this.selectedCityName = place.name;
+          this.selectedLat = place.geometry.location.lat();
+          this.selectedLng = place.geometry.location.lng();
+        }
+      });
+    });
+  }
+
+  addCity(): void {
+    if (this.selectedCityName && this.selectedLat && this.selectedLng) {
+      this.cityService.addCity(
+        this.selectedCityName,
+        this.selectedLat,
+        this.selectedLng
+      );
+
+      this.searchBox.nativeElement.value = '';
+      this.selectedCityName = '';
+      this.selectedLat = 0;
+      this.selectedLng = 0;
+    }
+  }
+}
--- WeatherApp/src/app/weather-for-location/weather-for-location.component.ts
+++ WeatherApp/src/app/weather-for-location/weather-for-location.component.ts
@@ -3,9 +3,10 @@ import { ActivatedRoute, RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { LeftPanelComponent } from "../left-panel/left-panel.component";
 import { DayHighlightsComponent } from "../day-highlights/day-highlights.component";
+import { CityContainerComponent } from '../city-container/city-container.component';
 @Component({
   selector: 'app-weather-for-location',
-  imports: [ButtonModule, LeftPanelComponent, DayHighlightsComponent],
+  imports: [ButtonModule, LeftPanelComponent, DayHighlightsComponent, CityContainerComponent],
   templateUrl: './weather-for-location.component.html',
   styleUrl: './weather-for-location.component.scss'
 })
